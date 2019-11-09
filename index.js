7ce72c66dcafe0b0a5af44d260c6072269b17606
yatsukiko <yatsu@yatsukiko.me>
2019-11-09 12:29:44 +0000

    tank u diisukodo - skip this commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = class jingle extends Plugin {
 		await waitFor(`.container-3baos1`) //waits till this shows up then do rest? idk
 			this.audio = document.createElement("audio")
 			this.audio.autoplay = true
-			this.audio.src = "https://yatsukiko.me/u/jingle.mp3" //source file, idk how to make it local 
+			this.audio.src = "/assets/773745b4ebae9f47e802724ec33b8a3f.mp3" //source file, idk how to make it local 
 			this.audio.volume = 0.2 //change from 0-1
 		
 	}
