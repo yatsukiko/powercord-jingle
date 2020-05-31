ba6433cd02985d4e605f7c7b6239dce5e1cb03b5
Melmsie <memyselfandlie@gmail.com>
2020-05-31 16:13:54 -0400

    test idk

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = class jingle extends Plugin {
 		await waitFor(`.container-3baos1`) //waits till this shows up then do rest? idk
 			this.audio = document.createElement("audio")
 			this.audio.autoplay = true
-			this.audio.src = "/assets/773745b4ebae9f47e802724ec33b8a3f.mp3" //source file, idk how to make it local 
-			this.audio.volume = 0.2 //change from 0-1
+			this.audio.src = "https://github.com/melmsie/powercord-jingle/blob/master/audio/ok.mp3" //source file, idk how to make it local 
+			this.audio.volume = 0.4 //change from 0-1
 		
 	}
 }
